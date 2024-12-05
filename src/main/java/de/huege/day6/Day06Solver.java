16a43262c7cacf7c960250d3ba53db6a9587e705
Jan Huege <jan.huege@istec.de>
2024-12-05 09:28:20 +0000

    prep day6

---
+++ src/main/java/de/huege/day6/Day06Solver.java
@@ -0,0 +1,23 @@
+package de.huege.day6;
+
+import de.huege.helpers.Day;
+
+public class Day06Solver extends Day {
+
+    public Day06Solver() {
+        super(6);
+    }
+
+    @Override
+    public long solvePart1(boolean isExample) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'solvePart1'");
+    }
+
+    @Override
+    public long solvePart2(boolean isExample) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'solvePart2'");
+    }
+    
+}
---
+++ src/test/java/Day6.java
@@ -0,0 +1,18 @@
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import de.huege.day6.Day06Solver;
+
+public class Day6 {
+
+    @Test
+    public void shouldSolvePart1() {
+        assertThat(new Day06Solver().solvePart1(true)).isEqualTo();
+    }
+
+    @Test
+    public void shouldSolvePart2() {
+        assertThat(new Day06Solver().solvePart2(true)).isEqualTo();
+    }
+}
