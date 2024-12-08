8d3877638fecdd26f095ed6540dbaa341add301d
Jan Huege <jan.huege93@gmail.com>
2024-12-08 11:13:53 +0000

    prep d9

---
+++ src/main/java/de/huege/day9/Day09Solver.java
@@ -0,0 +1,23 @@
+package de.huege.day9;
+
+import de.huege.helpers.Day;
+
+public class Day09Solver extends Day {
+
+    public Day09Solver() {
+        super(9);
+    }
+
+    @Override
+    public long solvePart1(boolean isExample) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'solvePart1'");
+    }
+
+    @Override
+    public long solvePart2(boolean isExample) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'solvePart2'");
+    }
+    
+}
---
+++ src/test/java/Day9.java
@@ -0,0 +1,18 @@
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import de.huege.day9.Day09Solver;
+
+public class Day9 {
+
+    @Test
+    public void shouldSolvePart1() {
+        assertThat(new Day09Solver().solvePart1(true)).isEqualTo(-1);
+    }
+
+    @Test
+    public void shouldSolvePart2() {
+        assertThat(new Day09Solver().solvePart2(true)).isEqualTo(-1);
+    }
+}
