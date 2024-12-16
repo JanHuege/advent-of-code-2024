00043820a08649f89c0d6f31f73123e3e0dd842e
Jan Huege <jan.huege93@gmail.com>
2024-12-16 10:37:05 +0000

    chore

--- src/main/java/de/huege/day14/Day14Solver.java
+++ src/main/java/de/huege/day14/Day14Solver.java
@@ -7,8 +7,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import de.huege.helpers.Day;
